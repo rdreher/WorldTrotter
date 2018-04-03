8f288f1661c92cbbdf5f3309e85dc845d0bbf873
Rafael Dreher <dreher@me.com>
2018-04-02 23:47:18 -0700

    Implements the unit conversation, delegates and validation

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Rafael Dreher on 4/2/18.
+//  Copyright © 2018 Rafael Dreher. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 2
+        return nf
+    }()
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text =
+                numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // User pressed the delete-key to remove a character, this is always valid, return true to allow change
+        if string.isEmpty { return true }
+        
+        // Build the full current string: TextField right now only contains the
+        // previous valid value. Use provided info to build up the new version.
+        // Can't just concat the two strings because the user might've moved the
+        // cursor and delete something in the middle.
+        let currentText = textField.text ?? ""
+        let replacementText = (currentText as NSString).replacingCharacters(in: range, with: string)
+        
+        // Use our string extensions to check if the string is a valid double and
+        // only has the specified amount of decimal places.
+        return replacementText.isValidDouble(maxDecimalPlaces: 2)
+    }
+    
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: . fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateCelsiusLabel()
+        textField.delegate = self
+    }
+}
+
+extension String {
+    func isValidDouble(maxDecimalPlaces: Int) -> Bool {
+        // Use NumberFormatter to check if we can turn the string into a number
+        // and to get the locale specific decimal separator.
+        let formatter = NumberFormatter()
+        formatter.allowsFloats = true // Default is true, be explicit anyways
+        let decimalSeparator = formatter.decimalSeparator ?? "."  // Gets the locale specific decimal separator. If for some reason there is none we assume "." is used as separator.
+        
+        // Check if we can create a valid number. (The formatter creates a NSNumber, but
+        // every NSNumber is a valid double, so we're good!)
+        if formatter.number(from: self) != nil {
+            // Split our string at the decimal separator
+            let split = self.components(separatedBy: decimalSeparator)
+            
+            // Depending on whether there was a decimalSeparator we may have one
+            // or two parts now. If it is two then the second part is the one after
+            // the separator, aka the digits we care about.
+            // If there was no separator then the user hasn't entered a decimal
+            // number yet and we treat the string as empty, succeeding the check
+            let digits = split.count == 2 ? split.last ?? "" : ""
+            
+            // Finally check if we're <= the allowed digits
+            return String(digits).count <= maxDecimalPlaces
+        }
+        
+        return false // couldn't turn string into a valid number
+    }
+}
